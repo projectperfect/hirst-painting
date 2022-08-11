e1459278aad15d2299c9c0fbc474244b8ec569cd
projectperfect <jprfct@protonmail.com>
2022-08-11 00:18:17 -0500

    loop for turtle class.

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ tim.forward(300)
 tim.setheading(0)
 number_of_dots = 100
 
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
